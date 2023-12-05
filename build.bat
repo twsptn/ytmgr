f398dfb7dbd2f7605f922d307b33079816dd8b62
twsptn <152618266+twsptn@users.noreply.github.com>
2023-12-05 12:12:46 +0800

    add windows build file

---
+++ build.bat
@@ -0,0 +1,10 @@
+echo on
+
+
+go build cmd/dataPrep/dataPrep.go
+
+go build cmd/drive/drive.go
+
+go build cmd/youtube/youtube.go
+
+
---
+++ clean.bat
@@ -0,0 +1,15 @@
+echo on
+
+
+IF EXIST dataPrep.exe (
+	del dataPrep.exe 
+)
+
+IF EXIST drive.exe (
+	del drive.exe 
+)
+
+IF EXIST youtube.exe (
+	del youtube.exe 
+)
+
